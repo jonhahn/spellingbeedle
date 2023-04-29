7f43c6fd259e8dd51762352d62c39d9d35f73dca
Jonathan Hahn <hahnx240@umn.edu>
2023-04-29 17:39:53 -0500

    Better colors

--- js/functions.js
+++ js/functions.js
@@ -80,16 +80,13 @@ finish = function(){
     response = success[total];
     response = response[Math.floor(Math.random() * response.length)];
 
-    var colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
-		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
-		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+    var colorArray = [
+		  '#809900', '#9900B3', '#E6B333', '#3366E6', '#B34D4D',
+		  '#80B300', '#6680B3', '#66991A', '#E6331A',
 		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
-		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
-		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
-		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+		  '#66664D', '#991AFF', '#E666FF', '#E666B3', '#33991A',
+		  '#4D8066', '#809940', '#999933', '#FF3380', '#4D80CC',
+		  '#E64D66', '#4D6380', '#FF4D4D', '#6666CC'];
 
     color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
